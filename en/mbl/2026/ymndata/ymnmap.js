4c4ac4aa775cb510bb0e214add89d6a27d03deac
BB <me@yomama.com>
2026-09-11 20:41:53 +0300

    -

--- en/mbl/2026/ymndata/ymnmap.js
+++ en/mbl/2026/ymndata/ymnmap.js
@@ -41,7 +41,7 @@ function plot(before,after) {
 }
 
 function init() {
-    map = L.map('map').setView([15.1027, 45.203], 9);
+    map = L.map('map').setView([15.1027, 45.203], 5);
 
     L.tileLayer('https://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
 	maxZoom: 20,
@@ -50,3 +50,4 @@ function init() {
     }).addTo(map);
 
 }
+   
